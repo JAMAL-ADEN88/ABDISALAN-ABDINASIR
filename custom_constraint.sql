24a127ff4498459a3079c25d0c9f7e2c384f8062
JAMAL-ADEN88 <joined2009@gmail.com>
2025-01-09 23:45:15 +0300

    Final submission
    cw dbs Q1 part i) proposed Custom Constraints

---
+++ custom_constraint.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 09, 2025 at 04:39 PM
+-- Server version: 9.1.0
+-- PHP Version: 8.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `custom_constraint`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `agents_table`
+--
+
+DROP TABLE IF EXISTS `agents_table`;
+CREATE TABLE IF NOT EXISTS `agents_table` (
+  `AgentID` int NOT NULL,
+  `Name` varchar(100) NOT NULL,
+  `ContactNumber` varchar(15) DEFAULT NULL,
+  `CommissionRate` decimal(5,2) DEFAULT NULL,
+  PRIMARY KEY (`AgentID`),
+  UNIQUE KEY `ContactNumber` (`ContactNumber`)
+) ;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
